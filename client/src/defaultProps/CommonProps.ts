02461e33ec8982f663fcf421cc66ea1bea87edc7
woorim960 <woorimprog@gmail.com>
2022-09-11 01:41:25 +0900

    [Remove] remove all of client files

--- client/src/defaultProps/CommonProps.ts
+++
@@ -1,163 +0,0 @@
-export const techStacksDefault = [
-  {
-    nameOption: {
-      name: "HTML",
-      logoName: "HTML-5",
-    },
-    progressBarOption: {
-      rate: "95%",
-      colorTo: "#C3C7DE",
-      colorFrom: "#C3C7DE",
-      height: "40px",
-    },
-  },
-  {
-    nameOption: {
-      name: "CSS",
-      logoName: "CSS-3",
-    },
-    progressBarOption: {
-      rate: "99%",
-      colorTo: "#544F7C",
-      colorFrom: "#544F7C",
-      height: "40px",
-    },
-  },
-  {
-    nameOption: {
-      name: "Javascript",
-      logoName: "Javascript",
-    },
-    progressBarOption: {
-      rate: "90%",
-      colorTo: "#C3C7DE",
-      colorFrom: "#C3C7DE",
-      height: "40px",
-    },
-  },
-  {
-    nameOption: {
-      name: "Typescript",
-      logoName: "Typescript-icon",
-    },
-    progressBarOption: {
-      rate: "80%",
-      colorTo: "#544F7C",
-      colorFrom: "#544F7C",
-      height: "40px",
-    },
-  },
-  {
-    nameOption: {
-      name: "React",
-      logoName: "React",
-    },
-    progressBarOption: {
-      rate: "85%",
-      colorTo: "#544F7C",
-      colorFrom: "#544F7C",
-      height: "40px",
-    },
-  },
-  {
-    nameOption: {
-      name: "Nextjs",
-      logoName: "Nextjs",
-    },
-    progressBarOption: {
-      rate: "80%",
-      colorTo: "#C3C7DE",
-      colorFrom: "#C3C7DE",
-      height: "40px",
-    },
-  },
-  {
-    nameOption: {
-      name: "React-Redux",
-      logoName: "Redux",
-    },
-    progressBarOption: {
-      rate: "40%",
-      colorTo: "#C3C7DE",
-      colorFrom: "#C3C7DE",
-      height: "40px",
-    },
-  },
-  {
-    nameOption: {
-      name: "Sass | Scss",
-      logoName: "Sass",
-    },
-    progressBarOption: {
-      rate: "90%",
-      colorTo: "#544F7C",
-      colorFrom: "#544F7C",
-      height: "40px",
-    },
-  },
-];
-
-export const introOptionDefault = {
-  title: "",
-  shortIntro: "세상에 긍정적인 변화를 일으킬 개발자 입니다.",
-  description:
-    "안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n\n잘부탁드립니다^^",
-};
-
-export const historyListDefault = [
-  {
-    startDate: "2021.01",
-    endDate: "2021.02",
-    title: "미식의 시대",
-    des: "스타트업 인턴십\n\n약 6주간 인턴십을 진행했습니다.\n주로 SQL, QA업무를 진행했습니다.",
-  },
-  {
-    startDate: "2021.05",
-    endDate: "2022.01",
-    title: "모던 애자일",
-    des: "서비스 개발\n\n프론트앤드 개발자로 활동하며 서비스를 개발했습니다.\n애자일 협업방식을 따르며 체계적인 개발을 진행했습니다.",
-  },
-];
-
-const channelOptionDefault = [
-  {
-    name: "github",
-    redirectUrl: "https://github.com/seohyunsim",
-    color: "black",
-  },
-  {
-    name: "velog",
-    redirectUrl: "https://velog.io/@seohyunsim",
-    color: "#20C997",
-  },
-  {
-    name: "instagram",
-    color: "#E1406C",
-  },
-];
-
-export const contactOptionDefault = {
-  title: "저에 대해 더 궁금하신가요?",
-  subTitle: "주저하지 말고 지금 바로 📧📲",
-  email: "ssh4563@naver.com",
-  buttonText: "Come here to send mail !",
-  channels: channelOptionDefault,
-  aboutMeInfos: [
-    {
-      title: "Residence",
-      description: "Seoul, Republic of Korea",
-    },
-    {
-      title: "Birth",
-      description: "2000. 11. 20",
-    },
-    {
-      title: "MBTI",
-      description: "ESFP || ENFP",
-    },
-    {
-      title: "Phone",
-      description: "010 9572 3357",
-    },
-  ],
-};
