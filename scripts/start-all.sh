82864cf2f8e728763d0eddb0bfc31327d5de370b
김지수 <kimjisu3817@gmail.com>
2022-10-02 22:46:35 +0900

    add: Separation of application execution scripts by OS #1

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -1,1 +1,11 @@
-npm run build --prefix ./server/app && docker-compose -f ./server/docker-compose.yml --env-file ./config/.server.env up -d && npm run start:client
+CHECK_OS="`uname -s`"
+
+if [[ ${CHECK_OS} = "Darwin"* ]]; then
+  npm run build --prefix ./server/app && docker-compose -f ./server/docker-compose.yml --env-file ./config/.server.env up -d && npm run start:client
+elif [[ ${CHECK_OS} = "MINGW32"* ]]; then
+  npm run start:windows
+elif [[ ${CHECK_OS} = "MINGW64"* ]]; then
+  npm run start:windows
+elif [[ ${CHECK_OS} = "CYGWIN"* ]]; then
+  npm run start:windows
+fi
