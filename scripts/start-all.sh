26dcbffc70ccf9e292d56259fa88be1b21f34b0b
김지수 <kimjisu3817@gmail.com>
2022-10-03 18:53:03 +0900

    add: Add linux application execution script #1

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -2,6 +2,8 @@ CHECK_OS="`uname -s`"
 
 if [[ ${CHECK_OS} = "Darwin"* ]]; then
   npm run build --prefix ./server/app && docker-compose -f ./server/docker-compose.yml --env-file ./config/.server.env up -d && npm run start:client
+elif [[ "$CHECK_OS" = "Linux"* ]]; then
+  npm run build --prefix ./server/app && docker-compose -f ./server/docker-compose.yml --env-file ./config/.server.env up -d && npm run start:client
 elif [[ ${CHECK_OS} = "MINGW32"* ]]; then
   npm run start:windows
 elif [[ ${CHECK_OS} = "MINGW64"* ]]; then
