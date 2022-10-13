4a08a5e43f4a838b5638745757abea65bdfb7201
김지수 <kimjisu3817@gmail.com>
2022-10-14 00:48:38 +0900

    [Add] Added typedoc for error return #16

--- server/app/src/apis/module/error.ts
+++ server/app/src/apis/module/error.ts
@@ -5,6 +5,12 @@ import {
 } from '../../service/error';
 import { Response } from 'express';
 
+/**
+ * @description Response branch processing logic for each error code
+ * @param {unknown | ServerError | BadRequestError | NotFoundError} err 
+ * @param {Response} res 
+ * @returns 
+ */
 function errorResposne(
   err: unknown | ServerError | BadRequestError | NotFoundError,
   res: Response
