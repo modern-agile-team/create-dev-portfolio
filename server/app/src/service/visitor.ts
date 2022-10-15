27ed1e8f4fe7a4f0fb4e4315e62b52ef16e83211
김지수 <kimjisu3817@gmail.com>
2022-10-15 16:24:30 +0900

    [Update] Add Visitor class typedoc #15

--- server/app/src/service/visitor.ts
+++ server/app/src/service/visitor.ts
@@ -1,4 +1,4 @@
-import { VisitorCmtDto, VisitorCmtEntity } from '../apis/visitor/visitor';
+import { VisitorCmtDto, VisitorCmtEntity, VisitorDto } from '../apis/visitor/visitor';
 import VisitorRepository from '../model/visitorRepository';
 import bcrypt from 'bcrypt';
 import { NotFoundError, ServerError } from './error';
@@ -9,12 +9,12 @@ interface Response {
   msg: string;
 }
 
-/**
- * @constructor
- * @param {VisitorRepository} visitorRepository
- * @param {any=} body
- */
+
 class Visitor {
+  /**
+   * @param {VisitorRepository} visitorRepository
+   * @param {any=} body
+   */
   private readonly visitorRepository: VisitorRepository;
   readonly body;
   constructor(visitorRepository: VisitorRepository, body?: any) {
@@ -22,11 +22,19 @@ class Visitor {
     this.body = body;
   }
 
+  /**
+   * @description Method for retrieving number of visitors
+   * @returns {VisitorDto} visitorCnt
+   */
   async getVisitorCnt() {
     const visitorCnt = await this.visitorRepository.getVisitorCnt();
     return visitorCnt;
   }
 
+  /**
+   * @description Update the number of visitors every time they visit your portfolio web
+   * @returns {VisitorDto} visitorCnt
+   */
   async updateAndGetVisitorCnt() {
     const todayDate = await this.visitorRepository.getVisitorTodayDate();
     const formatTodayDate = moment(todayDate, 'YYYY-MM-DD');
@@ -50,6 +58,9 @@ class Visitor {
     throw new ServerError('Interval server error');
   }
 
+  /**
+   * @description Create visitor comment information
+   */
   async createComment(): Promise<number> {
     const { body } = this;
     const encryptedPassword = await this.encryptPassword(body.password);
@@ -64,10 +75,17 @@ class Visitor {
       visitorComment
     );
 
-    if (commentId) return commentId;
+    if (commentId) {
+      return commentId;
+    }
     throw new ServerError('Interver Server Error');
   }
 
+  /**
+   * @description Method for password encryption
+   * @param {string} password
+   * @returns {Promise<string>}
+   */
   private async encryptPassword(password: string): Promise<string> {
     const saltRounds = 10;
 
@@ -80,6 +98,10 @@ class Visitor {
     return encryptedPassword;
   }
 
+  /**
+   * @param {number} visitorCommentId update target id
+   * @returns {Promise<Response>}
+   */
   async updateCommentById(visitorCommentId: number): Promise<Response> {
     const { password, description }: VisitorCmtDto = this.body;
 
@@ -87,15 +109,18 @@ class Visitor {
       visitorCommentId
     );
 
-    if (!visitorComment) throw new NotFoundError('No data exists');
+    if (!visitorComment) {
+      throw new NotFoundError('No data exists');
+    }
 
     const isSamePassword = await this.comparePassword(
       password,
       visitorComment.password
     );
 
-    if (!isSamePassword)
+    if (!isSamePassword) {
       return { success: false, msg: 'Passwords do not match' };
+    }
 
     await this.visitorRepository.updateVisitorComment(
       visitorCommentId,
@@ -105,28 +130,46 @@ class Visitor {
     return { success: true, msg: 'Visitor comment update complete' };
   }
 
+  /**
+   * @param {string} password 
+   * @param {string} encryptedPassword 
+   * @returns {Promise<boolean>} password Verification
+   */
   private async comparePassword(password: string, encryptedPassword: string) {
     return await bcrypt.compare(password, encryptedPassword);
   }
 
+  /**
+   * @description All visitor comment list retrieval methods
+   * @returns {Promise<{ visitorComments: VisitorCmtEntity[] }>}
+   */
   async getVisitorComments(): Promise<{ visitorComments: VisitorCmtEntity[] }> {
     const visitorComments = await this.visitorRepository.getVisitorComments();
 
     return { visitorComments };
   }
 
+  /**
+   * @param {number} visitorCommentId visitor comment id you want to delete 
+   * @returns {Promise<boolean>}
+   */
   async deleteVisitorCommentById(visitorCommentId: number): Promise<boolean> {
     const visitorComment = await this.visitorRepository.getVisitorCommentById(
       visitorCommentId
     );
 
-    if (!visitorComment) throw new NotFoundError('No data exists');
+    if (!visitorComment) {
+      throw new NotFoundError('No data exists');
+    }
 
     const isDelete = await this.visitorRepository.deleteVisitorCommentById(
       visitorCommentId
     );
 
-    if (isDelete) return true;
+    if (isDelete) {
+      return true;
+    }
+
     throw new ServerError('Interver Server Error');
   }
 }
